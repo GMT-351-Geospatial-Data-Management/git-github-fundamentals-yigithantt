f0f9a4cd801c7414cc42dfca46b5706897eb24d1
yigithantt <121801140+yigithantt@users.noreply.github.com>
2023-01-22 20:44:49 +0000

    index added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,24 @@ const router = require('express').Router()
 const pool = require('../database-connection')
 
 router.get('/', (req, res) => {
-    res.status(200).send({ date: Date.now() })
+    const fillObject = { 
+        communities: [],
+        events: []
+    }
+
+    pool.query(`select * from communities`, (err, dbRes) => {
+        dbRes.rows.forEach(element => {
+            fillObject.communities.push(element)
+        });
+    })
+    
+    pool.query(`select * from events`, (err, dbRes) => {
+        dbRes.rows.forEach(element => {
+            fillObject.events.push(element)
+        });
+    })
+
+    res.status(200).send(fillObject)
 })
 
 module.exports = router
