2b0a1969df33d9f1cf78cfd0cb24b253f43243cf
yigithantt <121801140+yigithantt@users.noreply.github.com>
2023-01-22 15:51:11 +0000

    postgresql eklendi

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const indexRouter = require('./routes/index')
 const eventsRouter = require('./routes/events')
 const communitiesRouter = require('./routes/communities')
 
+const pool = require('./database-connection')
+
 const app = express()
 
-app.use(indexRouter)
-app.use(eventsRouter)
-app.use(communitiesRouter)
+app.use('/', indexRouter)
+app.use('/events', eventsRouter)
+app.use('/communities', communitiesRouter)
 
 app.listen(8000, () => {
     console.log('server started')
